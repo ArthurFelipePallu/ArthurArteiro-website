dfa00eba108ede41dc8f66bae40db4f6c31678d6
Arthur Pallu <arthurfelipepallu@gmail.com>
2022-09-27 15:09:52 -0300

    novidades

---
+++ Javascript/InfoCard.js
@@ -0,0 +1,9 @@
+/* Open the sidenav */
+function openInfo(info) {
+    document.getElementById(info).style.width = "100%";
+  }
+  
+  /* Close/hide the sidenav */
+  function closeInfo(info) {
+    document.getElementById(info).style.width = "0";
+  }
